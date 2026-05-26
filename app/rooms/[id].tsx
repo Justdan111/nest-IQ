518532796f5e6747ecf4982cfb57602363463223
justdan111 <emmanuelngulube292@gmail.com>
2026-05-26 19:30:16 +0100

    refactor: simplify import statements in RoomDetailsScreen for cleaner code

--- app/rooms/[id].tsx
+++ app/rooms/[id].tsx
@@ -1,11 +1,6 @@
 import { useState } from 'react';
 import {
-  Alert,
-  Image,
-  Modal,
-  Pressable,
-  ScrollView,
-  Text, TextInput, View, } from 'react-native';
+  Alert, Image, Modal, Pressable, ScrollView, Text, TextInput, View, } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Ionicons } from '@expo/vector-icons';
 import { useLocalSearchParams, useRouter } from 'expo-router';
